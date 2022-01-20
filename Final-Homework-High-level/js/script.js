714661183f60bd4fa3971a35c27cf2dc55094a91
Plu33ha <74545611+Plu33ha@users.noreply.github.com>
2022-01-20 17:21:16 +0300

    finished html,css sctructure. Left to make adaptive and format

--- Final-Homework-High-level/js/script.js
+++ Final-Homework-High-level/js/script.js
@@ -1,5 +1,43 @@
+// burger menu
 let burger = document.querySelector("#burger-icon");
             let header = document.querySelector("#header");
             burger.onclick = function () {
                 header.classList.toggle("menu-open");
             };
+
+// button to top
+
+$(function() {
+
+ $(window).scroll(function() {
+
+ if($(this).scrollTop() != 0) {
+
+ $('#toTop').fadeIn();
+ 
+ } else {
+
+ $('#toTop').fadeOut();
+
+ }
+
+ });
+ 
+ $('#toTop').click(function() {
+
+ $('body,html').animate({scrollTop:0},800);
+
+ });
+
+
+ // changing themes
+ $('.portfolio__cases').each(function(){
+  $('.portfolio__case', this).each(function(i){
+    $(this).addClass('portfolio__case' + (i+1))
+  })
+})
+
+
+
+ 
+ 
